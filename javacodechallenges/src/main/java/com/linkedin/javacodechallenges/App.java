d26afc5f49f2d9d64c0e96cc6854d48adc48658d
dnlpalestina <daniel.palestina@globant.com>
2024-04-24 13:09:40 -0600

    Implement Person class

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,11 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+    public static void main(String[] args) {
+        Person p1 = new Person("Ron", "Freeman", 37);
+        Person p2 = new Person("Dan", "Palestina", 39);
+
+        p1.introduceYourself();
+        p2.introduceYourself();
     }
 }
