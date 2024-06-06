bb8966b2808b87fe847c22fedd234e6ebe8a3def
dnlpalestina <daniel.palestina@globant.com>
2024-06-06 15:42:19 -0600

    isPasswordComplex solution

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -7,9 +7,16 @@ public class App {
 
     public static boolean isPasswordComplex(String password) {
         return password.length() >= 6
-        && password.matches(".*\\d.*")
-        && password.matches(".*[a-z].*")
-        && password.matches(".*[A-Z].*");
+                && password.matches(".*\\d.*")
+                && password.matches(".*[a-z].*")
+                && password.matches(".*[A-Z].*");
+    }
+
+    public static boolean isPasswordComplexFunctional(String s) {
+        return s.length() >= 6 &&
+                s.chars().anyMatch(Character::isUpperCase) &&
+                s.chars().anyMatch(Character::isLowerCase) &&
+                s.chars().anyMatch(Character::isDigit);
     }
 
     public static void main(String[] args) {
